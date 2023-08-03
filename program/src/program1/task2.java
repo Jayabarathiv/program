51a8f1454fb1b3f2bb5e37968837e37c865c403c
jayabarathi v <jayabarathijayabarathi59@gmail.com>
2023-08-03 14:02:31 +0530

    star program

---
+++ program/src/program1/task2.java
@@ -0,0 +1,28 @@
+package program1;
+
+public class task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		int i, j, row=6;   
+		//outer loop for rows  
+		for(i=0; i<row; i++)   
+		{   
+		//inner loop for columns  
+		for(j=0; j<=i; j++)   
+		{   
+		//prints stars   
+		System.out.print("* ");   
+		}   
+		//throws the cursor in a new line after printing each line  
+		System.out.println();   
+		}   
+		}   
+		
+
+
+	}
+
+
